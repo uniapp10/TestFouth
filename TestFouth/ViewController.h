d83569e34b2d6cc6d3b08c8190de0773921aa14b
zhudong <zhudong10@163.com>
2017-11-01 11:55:23 +0800

    Initial Commit

---
+++ TestFouth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestFouth
+//
+//  Created by 朱冬冬 on 2017/11/1.
+//  Copyright © 2017年 朱冬冬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestFouth/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestFouth
+//
+//  Created by 朱冬冬 on 2017/11/1.
+//  Copyright © 2017年 朱冬冬. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
